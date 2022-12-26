6f1d728722a9a81e2de33fac4733342e6587de70
Mirtineh <mirfil92@gmail.com>
2022-12-26 15:45:32 +0300

    add styles to config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="text-red-600">Hello</div>;
+  return (
+    <div className="flex justify-center items-center h-screen font-kumbh">
+      <div className="bg-orange max-w-7xl h-full w-full"></div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      orange: "hsl(26, 100%, 55%)",
+      "pale-orange": "hsl(25, 100%, 94%)",
+      "very-dark-blue": "hsl(220, 13%, 13%)",
+      "dark-grayish-blue": "hsl(219, 9%, 45%)",
+      "light-grayish-blue": "hsl(223, 64%, 98%)",
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
+    },
+    fontFamily: {
+      kumbh: ["Kumbh", "Sans"],
+    },
+    fontWeight: {
+      normal: 400,
+      bold: 700,
+    },
     extend: {},
   },
   plugins: [],
