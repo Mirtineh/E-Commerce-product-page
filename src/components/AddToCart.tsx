23296e9880e8d1f8f361647ddf09da0b927c39e2
Mirtineh <mirfil92@gmail.com>
2022-12-27 11:47:35 +0300

    Add to cart component markup finished

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,9 +1,15 @@
 import { FunctionComponent } from "react";
+import cartIcon from "../assets/images/icon-cart.svg";
 
 interface AddToCartProps {}
 
 const AddToCart: FunctionComponent<AddToCartProps> = () => {
-  return <div className="flex justify-center bg-orange p-3 rounded-md"></div>;
+  return (
+    <button className="flex justify-center items-center gap-4 bg-orange p-3 rounded-lg text-white border-none hover:bg-opacity-50 hover:cursor-pointer">
+      <img src={cartIcon} alt="" />
+      <p className="m-0 font-bold text-lg">Add to cart</p>
+    </button>
+  );
 };
 
 export default AddToCart;
